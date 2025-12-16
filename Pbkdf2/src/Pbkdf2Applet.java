af97342fd3596e118b33c7433f248efbe04da63a
Andrii Pylypenko <56958998+PylypAn@users.noreply.github.com>
2025-12-16 14:48:28 +0100

    Pbkdf2 now reads password and salt from apdu

--- Pbkdf2/src/Pbkdf2Applet.java
+++ Pbkdf2/src/Pbkdf2Applet.java
@@ -144,26 +144,14 @@ public class Pbkdf2Applet extends javacard.framework.Applet
         byte[]    apdubuf = apdu.getBuffer();
         short     dataLen = apdu.setIncomingAndReceive();
 
-        //"password"
-        m_ramArray1[0] = 0x70;
-        m_ramArray1[1] = 0x61;
-        m_ramArray1[2] = 0x73;
-        m_ramArray1[3] = 0x73;
-        m_ramArray1[4] = 0x77;
-        m_ramArray1[5] = 0x6F;
-        m_ramArray1[6] = 0x72;
-        m_ramArray1[7] = 0x64;
-        
-        //"salt"
-        m_ramArray2[0] = 0x73;
-        m_ramArray2[1] = 0x61;
-        m_ramArray2[2] = 0x6C;
-        m_ramArray2[3] = 0x74;
-        
-        //Util.arrayCopyNonAtomic(apdubuf, ISO7816.OFFSET_CDATA, m_ramArray1, (short) 0, dataLen);
-        short ret = m_pbkdf2.doFinal(m_ramArray1, (short) 0, (short) 8,
-                                     m_ramArray2, (short) 0, (short) 4,
-                                     (short) 1,
+        short pswLen = apdubuf[ISO7816.OFFSET_P1];
+        short saltLen = apdubuf[ISO7816.OFFSET_P2];
+        Util.arrayCopyNonAtomic(apdubuf, ISO7816.OFFSET_CDATA, m_ramArray1, (short) 0, pswLen);
+        Util.arrayCopyNonAtomic(apdubuf, (short) (ISO7816.OFFSET_CDATA + pswLen), m_ramArray2, (short) 0, saltLen);
+
+        short ret = m_pbkdf2.doFinal(m_ramArray1, (short) 0, pswLen,
+                                     m_ramArray2, (short) 0, saltLen,
+                                     (short) 2,
                                      m_ramArray3, (short) 0);
         
         // SEND OUTGOING BUFFER
